a03f4279b3f788d79e4aba3b1f66b2043ae77d0a
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-10-08 19:03:06 +0200

    finished js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const input = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
-    console.log(this.value);
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 input.forEach(input => input.addEventListener('change', handleUpdate))
