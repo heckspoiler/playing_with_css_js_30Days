a30a9d8cb8e8ae958465fa871a0831d695fec187
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-10-11 09:47:30 +0200

    added rotate

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ function handleUpdate() {
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
+
+
 input.forEach(input => input.addEventListener('change', handleUpdate))
 input.forEach(input => input.addEventListener('mousemove', handleUpdate))
